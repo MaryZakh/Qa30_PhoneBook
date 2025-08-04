bbd4ec92da8461311c454fac1638304fca6088ea
Maria Z <z0559882272@gmail.com>
2025-08-04 20:33:39 +0300

    added loginSuccess

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,14 +3,21 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class ApplicationManager {
 
     WebDriver wd;
+    HelperUser helperUser;
 
 
     public void init() {
         wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wd.navigate().to("https://telranedu.web.app/");
+        helperUser = new HelperUser(wd);
     }
 
 
@@ -18,5 +25,7 @@ public class ApplicationManager {
         wd.quit();
     }
 
-
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
 }
